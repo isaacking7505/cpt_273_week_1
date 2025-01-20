bf15d91159955df863694879cfa1298712c01dd1
Brett Bisesti <bisestinumber2@gmail.com>
2025-01-19 19:02:26 -0500

    Update app.py

--- app.py
+++ app.py
@@ -28,24 +28,13 @@ students = [
 def home():
     return "Welcome to the Flask API!"
 
-
-
-'''
-
-Please create 4 API endpoints
-
-GET /   - Gets all Students as JSON
-GET /<id> - gets student by ID as JSON object
-POST / - Add NEW student to the array above and returns 200 "added" if added
-PUT /<id> - Updates name of student given an ID
-DELETE /<id> - Deletes student from array by given ID
-
-Once your API has been created and tested, please publish it to GitHub and submit the URL of the public repository to Brightspace!
-
-'''
+#### ASSIGNMENT STARTS HERE ####
 
 
 
+
+
+#### ASSIGNMENT ENDS HERE ####
 
 if __name__ == '__main__':
     app.run(debug=True)
